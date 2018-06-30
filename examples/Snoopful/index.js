eb2365fef09824ac5de128bdb3e19738fa8653ac
Patrick Kempff <patrickkempff@gmail.com>
2018-06-30 19:35:57 +0200

    Fixed unresolved import

--- examples/Snoopful/index.js
+++ examples/Snoopful/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native'
 import React, { Component } from 'react'
 import { Text } from 'react-native'
-import App from './App'
+import App from './app'
 
 AppRegistry.registerComponent('Snoopful', () => App)
